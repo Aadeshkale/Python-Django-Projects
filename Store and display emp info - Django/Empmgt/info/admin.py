080ae3d2f2f6afd063560435ab4ef93749abc1f3
Aadeshkale <aadeshkale0@gmail.com>
2018-11-21 16:11:32 +0530

    names

--- Store and display emp info - Django/Empmgt/info/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Emp
-class AdminEmp(admin.ModelAdmin):
-    list_display=['eid','name','email','sal','loc']
-
-admin.site.register(Emp) # Register your models here.
